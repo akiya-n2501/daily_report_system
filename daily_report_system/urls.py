b779a4335d7b3e0bc69090a03dc29196eba3c4a3
akiya-n2501 <keisei.akiya@hit-inc.jp>
2025-04-23 09:46:13 +0900

    login機能

--- daily_report_system/urls.py
+++ daily_report_system/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("employees/", include("employees.urls")),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="accounts/login.html"),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
