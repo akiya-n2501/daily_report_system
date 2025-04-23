4dc61885336a5adaf3de8922209e587146f98fe1
akiya-n2501 <keisei.akiya@hit-inc.jp>
2025-04-23 12:08:34 +0900

    form, staff_member_erquired

--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404, redirect, render
 
+from .forms import EmployeeUserForm
 from .models import Employee
 
 
@@ -8,34 +9,31 @@ def home(request):
     return render(request, "home.html")
 
 
-@login_required
-def employee_create(request):
-    if request.method == "POST":
-        name = request.POST["name"]
-        email = request.POST["email"]
-        Employee.objects.create(name=name, email=email)
-        return redirect("employee_index")
-    return redirect("employee_new")
-
-
-@login_required
+@staff_member_required
 def employee_new(request):
-    return render(request, "employees/employee_new.html")
+    if request.method == "POST":
+        form = EmployeeUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("employee_index")
+    else:
+        form = EmployeeUserForm()
+    return render(request, "employees/employee_form.html", {"form": form})
 
 
-@login_required
+@staff_member_required
 def employee_list(request):
     employees = Employee.objects.all()
     return render(request, "employees/employee_list.html", {"employees": employees})
 
 
-@login_required
+@staff_member_required
 def employee_edit(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
     return render(request, "employees/employee_edit.html", {"employee": employee})
 
 
-@login_required
+@staff_member_required
 def employee_update(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
     if request.method == "POST":
@@ -46,13 +44,13 @@ def employee_update(request, pk):
     return redirect("employee_edit", pk=pk)
 
 
-@login_required
+@staff_member_required
 def employee_delete_confirm(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
     return render(request, "employees/employee_delete.html", {"employee": employee})
 
 
-@login_required
+@staff_member_required
 def employee_delete(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
     employee.delete()
