26c92943cb0d7fcdc4de1360fc7df3c0e88c21fa
yuhi.kondo <yuhi.kondo@hit-inc.jp>
2025-05-02 15:15:39 +0900

    日報コメントモデルの作成 日報コメントモデルの単体テスト

--- daily_reports/models.py
+++ daily_reports/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from employees.models import Employee
+
 
 # Create your models here.
+class DailyReportComment(models.Model):
+    employee = models.ForeignKey("employees.Employee")
+    daily_report = models.ForeignKey("DailyReport")
+    comment = models.TextField(max_length=2000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.employee.name} {self.daily_report.reported_on.date.today()} {self.comment[:7]})"
--- daily_reports/tests.py
+++ daily_reports/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import DailyReportComment
+from employees.models import Employee, User
+
 
 # Create your tests here.
+
+
+# modelの単体テスト
+class DailyReportCommentTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="password")
+        self.employee = Employee.objects.create(
+            name="Alice", email="alice@example.com", department="HR", user=self.user
+        )
+        self.dailyReport = DailyReport.objects.create()
+
+    def test_report_comment_str_with_user(self):
+        daily_report_comment = DailyReportComment.objects.create(
+            employee=self.employee,
+            daily_report=self.dailyReport,
+            comment="コメントです。",
+        )
+        self.assertEqual(str(daily_report_comment), "Alice (コメントです。)")
