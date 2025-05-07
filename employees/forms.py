9ceab16bf1292dd2714a56efdfcfdfd9f421e8c4
yuhi.kondo <yuhi.kondo@hit-inc.jp>
2025-05-07 16:10:56 +0900

    ・従業員編集画面の準備

--- employees/forms.py
+++ employees/forms.py
@@ -24,3 +24,20 @@ class EmployeeUserForm(forms.ModelForm):
 
 
 # TODO 編集用フォームを作成
+class EmployeeUserEditForm(forms.ModelForm):
+    username = forms.CharField(max_length=150, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
+
+    class Meta:
+        model = Employee
+        fields = ["name", "email", "department", "username", "password"]
+
+    def save(self, commit=True):
+        employee = super().save(commit=False)
+        username = self.cleaned_data["username"]
+        password = self.cleaned_data["password"]
+        user = User.objects.create_user(username=username, password=password)
+        employee.user = user
+        if commit:
+            employee.save()
+        return employee
--- employees/views.py
+++ employees/views.py
@@ -2,7 +2,8 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.decorators import method_decorator
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
+from django.urls import reverse_lazy
 
 from .forms import EmployeeUserForm
 from .models import Employee
@@ -36,22 +37,35 @@ class EmployeeListView(ListView):
         return context
 
 
+@method_decorator(staff_member_required, name="dispatch")
+class EmployeeUpdateView(UpdateView):
+    model = Employee
+    template_name = "employees/employee_form_edit.html"
+    form_class = EmployeeUserForm
+    success_url = reverse_lazy('employees:employee_edit')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["user"] = User.objects.select_related("user").values("is_staff")
+        return context
+
+
 # TODO Email, Username, Passwordを変更可能にする。
-@staff_member_required
-def employee_edit(request, pk):
-    employee = get_object_or_404(Employee, pk=pk)
-    return render(request, "employees/employee_edit.html", {"employee": employee})
+# @staff_member_required
+# def employee_edit(request, pk):
+#     employee = get_object_or_404(Employee, pk=pk)
+#     return render(request, "employees/employee_edit.html", {"employee": employee})
 
 
-@staff_member_required
-def employee_update(request, pk):
-    employee = get_object_or_404(Employee, pk=pk)
-    if request.method == "POST":
-        employee.name = request.POST["name"]
-        employee.email = request.POST["email"]
-        employee.save()
-        return redirect("employee_index")
-    return redirect("employee_edit", pk=pk)
+# @staff_member_required
+# def employee_update(request, pk):
+#     employee = get_object_or_404(Employee, pk=pk)
+#     if request.method == "POST":
+#         employee.name = request.POST["name"]
+#         employee.email = request.POST["email"]
+#         employee.save()
+#         return redirect("employee_index")
+#     return redirect("employee_edit", pk=pk)
 
 
 @staff_member_required
