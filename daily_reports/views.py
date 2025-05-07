298c76feb4ac8a94137e8fed043ebc22aa81ab4d
yusaiOfficial <yusai.masamoto@hit-inc.jp>
2025-05-07 15:01:03 +0900

    日報編集画面の作成

--- daily_reports/views.py
+++ daily_reports/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import DailyReport
+from django.urls import reverse_lazy
+from .forms import DailyReportForm
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+
+class DailyReportEditView(LoginRequiredMixin, UpdateView):
+    model = DailyReport
+    form_class = DailyReportForm
+    template_name = 'daily_reports/daily_report_edit.html'
+
+    success_url = reverse_lazy('daily_reports:daily_report_index')
+    def get_context_data(self, **kwargs):
+       context = super(DailyReportEditView, self).get_context_data(**kwargs)
+       context['message_type'] = "edit"
+       return context
