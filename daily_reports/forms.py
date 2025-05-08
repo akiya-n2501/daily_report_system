0b60fc1bf6157f215ec258a587ee9e58b0f0b459
akiya-n2501 <keisei.akiya@hit-inc.jp>
2025-05-08 11:06:02 +0900

    日付範囲フォームの作成

--- daily_reports/forms.py
+++ daily_reports/forms.py
@@ -1,3 +1,10 @@
 from django import forms
 
+
 class DateRangeForm(forms.Form):
+    start_date = forms.DateField(
+        widget=forms.DateInput(attrs={"type": "date"}), label="いつから"
+    )
+    end_date = forms.DateField(
+        widget=forms.DateInput(attrs={"type": "date"}), label="いつまで"
+    )
