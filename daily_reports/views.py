a4d7dc4f498bfc79cc40f5ac4cfad16c54caa6b2
akiya-n2501 <keisei.akiya@hit-inc.jp>
2025-05-08 09:17:17 +0900

    ログインしないと日報一覧画面に到達できないようにした

--- daily_reports/views.py
+++ daily_reports/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView
 
 from .models import DailyReport, Employee
 
 
 # 日報一覧
+@method_decorator(login_required, name="dispatch")
 class DailyReportListView(ListView):
     model = DailyReport
     template_name = "daily_reports/daily_report_list.html"
