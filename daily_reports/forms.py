f4e4f3154eda1da6b86ac500a1fd2aa5bf97d72c
hirohirota-13 <yutaro.hirota@hit-inc.jp>
2025-05-08 13:01:25 +0900

    日報新規登録画面の動作確認

--- daily_reports/forms.py
+++ daily_reports/forms.py
@@ -12,6 +12,14 @@ class DailyReportForm(forms.ModelForm):
                   "job_description": "業務内容",
                   "reported_on": "日付",
                   }
+    
+    
+    # def clean_job_description(self):
+    #      job_description = self.cleaned_data.get("job_description")
+    #      if len(job_description) > 2000:
+    #         raise forms.ValidationError("業務内容は2000文字以内で入力してください。")
+    #      return job_description
+
 
     def save(self, commit=True):
         daily_report = super().save(commit=False)
